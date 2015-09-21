3eb52824ecb705901d335d36680db1aaeb8801af
Melissa <mjblavos@gmail.com>
2015-09-21 13:56:08 -0400

    now all buttons work

--- popup.js
+++ popup.js
@@ -1,32 +1,27 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 var myUrl = urlFinder();
 
 function urlFinder () {
-	if ($("#button") === "one") {
-		return "http://app.bronto.com";
+	if ($("#button") == "one") {
+		console.log("http://app.bronto.com");
 	}
-	else if ($("#button") === "two") {
-		return "http://bronto.com/welcomekit";
+	else if ($("#button") == "two") {
+		console.log("http://bronto.com/welcomekit");
 	} else {
-		return "https://server.iad.liveperson.net/hc/77739214/?cmd=file&file=visitorWantsToChat&site=77739214&byhref=1&SESSIONVAR!skill=Lead%20Qualification&imageUrl=https://server.iad.liveperson.nethttps://hosting-source.bronto.com/7894/public/LivePerson/";
+		console.log("https://server.iad.liveperson.net/hc/77739214/?cmd=file&file=visitorWantsToChat&site=77739214&byhref=1&SESSIONVAR!skill=Lead%20Qualification&imageUrl=https://server.iad.liveperson.nethttps://hosting-source.bronto.com/7894/public/LivePerson/");
 	}
 }
 
-function awesome() {
+function click(e) {
   chrome.tabs.create({url: myUrl});
-}  
-
-function clickHandler(e) {
-  setTimeout(awesome, 1000);
 }
 
-function main() {
-  // Initialization work goes here.
-}
-
-// Add event listeners once the DOM has fully loaded by listening for the
-// `DOMContentLoaded` event on the document, and adding your listeners to
-// specific elements when it triggers.
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('button').addEventListener('click', clickHandler);
-  main();
+  var buttons = document.querySelectorAll('button');
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', click);
+  }
 });
---
+++ popup_OLD.js
@@ -0,0 +1,32 @@
+var myUrl = urlFinder();
+
+function urlFinder () {
+	if ($("#button") == "one") {
+		return "http://app.bronto.com";
+	}
+	else if ($("#button") == "two") {
+		return "http://bronto.com/welcomekit";
+	} else {
+		return "https://server.iad.liveperson.net/hc/77739214/?cmd=file&file=visitorWantsToChat&site=77739214&byhref=1&SESSIONVAR!skill=Lead%20Qualification&imageUrl=https://server.iad.liveperson.nethttps://hosting-source.bronto.com/7894/public/LivePerson/";
+	}
+}
+
+function awesome() {
+  chrome.tabs.create({url: myUrl});
+}  
+
+function clickHandler(e) {
+  setTimeout(awesome, 1000);
+}
+
+function main() {
+  // Initialization work goes here.
+}
+
+// Add event listeners once the DOM has fully loaded by listening for the
+// `DOMContentLoaded` event on the document, and adding your listeners to
+// specific elements when it triggers.
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('button').addEventListener('click', clickHandler);
+  main();
+});
