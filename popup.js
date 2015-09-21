37b89ab399e5764911c975719f312895b28ea249
Melissa <mjblavos@gmail.com>
2015-09-21 16:02:01 -0400

    added analytics

--- popup.js
+++ popup.js
@@ -36,3 +36,13 @@ document.addEventListener('DOMContentLoaded', function () {
     buttons[i].addEventListener('click', click);
   }
 });
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-30105-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
